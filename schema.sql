70f39e794cb06de29b221af7b0804dae2a514382
DohaKhamaiseh <dohakhamaiseh1999@gmail.com>
2023-03-14 09:27:08 +0000

    update the table

--- schema.sql
+++ schema.sql
@@ -5,5 +5,6 @@ CREATE TABLE IF NOT EXISTS MovieTable (
     title VARCHAR(255),
     release_date VARCHAR(255) ,
     poster_path VARCHAR (255),
-    overview  VARCHAR (255)
+    overview  VARCHAR (1000),
+    comment VARCHAR (1000)
 );
