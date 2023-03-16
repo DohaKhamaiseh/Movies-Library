81f2439eacfa9addfaf437c6c0148c57e04fc128
DohaKhamaiseh <dohakhamaiseh1999@gmail.com>
2023-03-16 18:41:38 +0000

    done

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS MovieTable;
 
 CREATE TABLE IF NOT EXISTS MovieTable ( 
-    id INT PRIMARY KEY, 
+    id SERIAL PRIMARY KEY, 
     title VARCHAR(255),
     release_date VARCHAR(255) ,
     poster_path VARCHAR (255),
